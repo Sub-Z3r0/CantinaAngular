4e86c91ffdbcd4221358f9e7fd92bdc26a8957eb
liverpoljer <liverpoljer@live.dk>
2018-12-14 11:04:58 +0100

    choose works

--- src/app/choose-main-page-items/choose-main-page-items.component.ts
+++ src/app/choose-main-page-items/choose-main-page-items.component.ts
@@ -60,29 +60,55 @@ export class ChooseMainPageItemsComponent implements OnInit {
   saveDailyChoice() {
     this.today = new Date;
     if (this.selected1 != null) {
+      this.selected1.FoodDate = this.today;
+      this.mainFoodService.UpdateToDaily(this.selected1)
+        .subscribe(() => {
+          this.router.navigateByUrl('');
+        });
     }
-    this.selected1.FoodDate = this.today;
-    console.log(this.selected1);
-    this.mainFoodService.UpdateToDaily(this.selected1)
-      .subscribe(() => {
-        this.router.navigateByUrl('');
-      });
+
     if (this.selected2 != null) {
+      this.selected2.FoodDate = this.today;
+      console.log(this.selected2);
+      this.mainFoodService.UpdateToDaily(this.selected2)
+        .subscribe(() => {
+          this.router.navigateByUrl('');
+        });
     }
-    this.selected2.FoodDate = this.today;
-    console.log(this.selected2);
-    this.mainFoodService.UpdateToDaily(this.selected2)
-      .subscribe(() => {
-        this.router.navigateByUrl('');
-      });
+
     if (this.selected3 != null) {
+      this.selected3.FoodDate = this.today;
+      console.log(this.selected3);
+      this.mainFoodService.UpdateToDaily(this.selected3)
+        .subscribe(() => {
+          this.router.navigateByUrl('');
+        });
     }
-    this.selected3.FoodDate = this.today;
-    console.log(this.selected3);
-    this.mainFoodService.UpdateToDaily(this.selected3)
-      .subscribe(() => {
-        this.router.navigateByUrl('');
-      });
+    if (this.selectedOffers4 != null) {
+      this.selectedOffers4.offersDate = this.today;
+      console.log(this.selectedOffers4);
+      this.specielOfferService.updateOffers(this.selectedOffers4)
+        .subscribe(() => {
+          this.router.navigateByUrl('');
+        });
+    }
+    if (this.selectedOffers5 != null) {
+      this.selectedOffers5.offersDate = this.today;
+      console.log(this.selectedOffers5);
+      this.specielOfferService.updateOffers(this.selectedOffers5)
+        .subscribe(() => {
+          this.router.navigateByUrl('');
+        });
+    }
+    if (this.selectedOffers6 != null) {
+      this.selectedOffers6.offersDate = this.today;
+      console.log(this.selectedOffers6);
+      this.specielOfferService.updateOffers(this.selectedOffers6)
+        .subscribe(() => {
+          this.router.navigateByUrl('');
+        });
+    }
+
   }
 
   }
